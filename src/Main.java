e7e7a63e7681187801f6ec14d8cadd5191df9cb7
Jozincanela <Afrodrigues2004@gmail.com>
2024-02-28 14:55:45 -0300

    Starting to implement Board and Piece

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import boardgame.Position;
 public class Main {
     public static void main(String[] args) {
 
-        Position pos = new Position(3,5);
-        System.out.print(pos);
+
         }
     }
